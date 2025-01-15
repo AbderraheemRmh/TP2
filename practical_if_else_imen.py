10ca1f843773180f6c4fa6e2ec5914c731ab18e3
imen <imen.hamdane.simoud@gmail.com>
2025-01-15 21:15:17 +0100

    tp2 if elif els

---
+++ practical_if_else_imen.py
@@ -0,0 +1,18 @@
+first_name = ""
+laste_name = ""
+nom = input("veuillez saisir votre nom svp : ")
+prenom = input ("veuillez saisir votre prenom svp : ")
+if nom == laste_name or prenom == first_name : # if not nom or not prenom :
+    print ("erreur : le nom et le prenom doivent etre indiques")
+else :
+    print ("bienvenue," + nom + " " + prenom + "!")
+user = "admin"
+login = "password123"
+nom_utilisateur = input("veuillez saisir votre nom d'utilisateur svp : ")
+mot_de_passe = input("veuillez saisir vote mot de passe svp : ")
+if nom_utilisateur == user and mot_de_passe == login :
+    print ("connexion reussie")
+elif nom_utilisateur != "admin" :
+    print ("votre nom d'utilisateur est incorrecte")
+else :
+    print ("votre mot de passe est incorrecte")
